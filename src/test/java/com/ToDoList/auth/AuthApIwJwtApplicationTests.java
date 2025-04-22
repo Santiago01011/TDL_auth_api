1c4bade4c56c643912d6f0d4ee2c20d3bb5ae538
Santiago <santiagozapata2002@gmail.com>
2025-04-22 12:17:14 -0300

    first commit

---
+++ src/test/java/com/ToDoList/auth/AuthApIwJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ToDoList.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApIwJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
