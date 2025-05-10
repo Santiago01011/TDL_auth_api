c199d851246e27efd2fd46daf0d2a2950a21402c
Santiago <santiagozapata2002@gmail.com>
2025-05-10 10:46:23 -0300

    props added for context failures

--- src/main/java/com/TrashTDL/ServerlessAuth/config/SpringContextHolder.java
+++ src/main/java/com/TrashTDL/ServerlessAuth/config/SpringContextHolder.java
@@ -4,6 +4,8 @@ import com.TrashTDL.ServerlessAuth.ServerlessAuthApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpringContextHolder {
     private static ConfigurableApplicationContext context;
@@ -13,8 +15,14 @@ public class SpringContextHolder {
         if (context == null) {
             synchronized (lock) {
                 if (context == null) {
+                    Map<String, Object> props = new HashMap<>();
+                    props.put("spring.datasource.url", System.getenv("SPRING_DATASOURCE_URL"));
+                    props.put("spring.datasource.username", System.getenv("SPRING_DATASOURCE_USERNAME"));
+                    props.put("spring.datasource.password", System.getenv("SPRING_DATASOURCE_PASSWORD"));
+
                     context = new SpringApplicationBuilder(ServerlessAuthApplication.class)
                                     .web(WebApplicationType.NONE)
+                                    .properties(props)
                                     .run();
                 }
             }
